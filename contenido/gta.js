ba16e5b31e0352a36a4f1041ad787866d146d446
camilo.amezquita <jcaopita@gmail.com>
2025-03-28 23:32:25 -0500

    index terminado

---
+++ contenido/gta.js
@@ -0,0 +1,8 @@
+let index = 0;
+function moveSlide() {
+    const slides = document.querySelectorAll(".carousel-container img");
+    index = (index + 1) % slides.length;
+    document.querySelector(".carousel-container").style.transform = `translateX(-${index * 100}%)`;
+}
+setInterval(moveSlide, 3000);
+
